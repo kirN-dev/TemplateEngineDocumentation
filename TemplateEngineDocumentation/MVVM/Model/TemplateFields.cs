79b19a9eaf2a868868ef05230d99e5e51321c071
kirN-dev <neekron666@gmail.com>
2022-03-06 12:39:38 +0300

    Added fields for tempalte

---
+++ TemplateEngineDocumentation/MVVM/Model/TemplateFields.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TemplateEngineDocumentation.MVVM.Model
+{
+    class TemplateFields
+    {
+        private string _pathFields;
+        public TemplateFields(string pathFields)
+        {
+            PathFields = pathFields;
+        }
+        public string PathFields
+        {
+            get => _pathFields;
+            set
+            {
+                if (!File.Exists(value))
+                {
+                    throw new ArgumentException(nameof(PathFields), "File not exists");
+                }
+
+                _pathFields = value;
+            }
+        }
+
+        public Dictionary<string, string> Fields { get; set; }
+    }
+}

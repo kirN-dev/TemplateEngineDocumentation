d1e162da1447960357d00db46b672bb19cdebfac
kirN-dev <neekron666@gmail.com>
2022-03-06 12:35:42 +0300

    Testing fill template docx

---
+++ TemplateEngineDocumentation/MVVM/Model/Templater.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using TemplateEngine.Docx;
+
+namespace TemplateEngineDocumentation.MVVM.Model
+{
+    class Templater
+    {
+        public static void AddContent()
+        {
+            File.Delete("OutputDocument.docx");
+            File.Copy("SRS.docx", "OutputDocument.docx");
+
+            var valuesToFill = new Content(
+                new FieldContent("", DateTime.Now.Date.ToString()),
+                new TableContent("Team members")
+                    .AddRow(
+                        new FieldContent("Full name", "Семёнов Илья Васильевич"),
+                        new FieldContent("Role", "Разработчик"))
+                    .AddRow(
+                        new FieldContent("Full name", "Петров Фёдор Анатольевич"),
+                        new FieldContent("Role", "Разработчик"))
+                    .AddRow(
+                        new FieldContent("Full name", "Артемьев Вячеслав Геннадьевич"),
+                        new FieldContent("Role", "Ведущий разработчик")),
+                new FieldContent("Count", "3")
+            );
+
+            using (var outputDocument = new TemplateProcessor("OutputDocument.docx")
+                .SetRemoveContentControls(true))
+            {
+                outputDocument.FillContent(valuesToFill);
+                outputDocument.SaveChanges();
+            }
+        }
+    }
+}
--- TemplateEngineDocumentation/MVVM/View/MainWindow.xaml.cs
+++ TemplateEngineDocumentation/MVVM/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TemplateEngineDocumentation.MVVM.Model;
 
 namespace TemplateEngineDocumentation
 {
@@ -23,6 +24,7 @@ namespace TemplateEngineDocumentation
         public MainWindow()
         {
             InitializeComponent();
+            Templater.AddContent();
         }
     }
 }

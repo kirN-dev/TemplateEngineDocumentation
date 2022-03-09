b1882807d2503adfaf5ae30fae3b551efd23404a
Kirill Novitskii_Home <Kirill_Novitskii@epam.com>
2022-03-09 19:36:09 +0300

    Added reads fields from a file

--- TemplateEngineDocumentation/MVVM/Model/TemplateFields.cs
+++ TemplateEngineDocumentation/MVVM/Model/TemplateFields.cs
@@ -1,30 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace TemplateEngineDocumentation.MVVM.Model
 {
-    class TemplateFields
-    {
-        private string _pathFields;
-        public TemplateFields(string pathFields)
-        {
-            PathFields = pathFields;
-        }
-        public string PathFields
-        {
-            get => _pathFields;
-            set
-            {
-                if (!File.Exists(value))
-                {
-                    throw new ArgumentException(nameof(PathFields), "File not exists");
-                }
+	class TemplateFields
+	{
+		private string _pathFields;
+		public TemplateFields(string pathFields)
+		{
+			PathFields = pathFields;
+		}
+		public List<string> Fields { get; private set; }
+		public string PathFields
+		{
+			get => _pathFields;
+			set
+			{
+				if (!File.Exists(value))
+				{
+					throw new ArgumentException(nameof(PathFields), "File not exists");
+				}
 
-                _pathFields = value;
-            }
-        }
+				_pathFields = value;
+			}
+		}
 
-        public Dictionary<string, string> Fields { get; set; }
-    }
+		public void SetFields()
+		{
+			using(StreamReader streamReader = new(PathFields))
+			{
+				Fields.AddRange(streamReader.ReadToEnd().Split('\n'));
+			}
+		}
+	}
 }
---
+++ TemplateEngineDocumentation/Utilites/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace TemplateEngineDocumentation.Utilites
+{
+	public class Configuration
+	{
+		private static string _connection;
+
+		public static string GetPathFileds()
+		{
+			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+				 .SetBasePath(Directory.GetCurrentDirectory())
+				 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+			var configuration = configurationBuilder.Build();
+			_connection = configuration["Connection"];
+			return _connection;
+		}
+	}
+}
